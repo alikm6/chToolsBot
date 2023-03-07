110c6fd86702477925e4455f6e4237ffa778dbbb
alikm6 <ali.karimi1377@gmail.com>
2023-03-08 01:59:08 +0330

    Remove unnecessary functions

--- functions/functions.php
+++ functions/functions.php
@@ -6,6 +6,5 @@ require realpath(__DIR__) . '/command.php';
 require realpath(__DIR__) . '/files.php';
 require realpath(__DIR__) . '/help.php';
 require realpath(__DIR__) . '/inlinekey.php';
-require realpath(__DIR__) . '/language.php';
 require realpath(__DIR__) . '/main.php';
 require realpath(__DIR__) . '/sendto.php';
