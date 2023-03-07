8fd9be939622efa427cfc1e843a95a42a106fc87
alikm6 <ali.karimi1377@gmail.com>
2023-03-08 02:04:08 +0330

    Add POMO requirements

--- includes.php
+++ includes.php
@@ -5,4 +5,6 @@ require realpath(__DIR__) . '/config.php';
 $loader = require realpath(__DIR__) . '/vendor/autoload.php';
 $loader->add('', __DIR__ . '/class');
 
+require realpath(__DIR__) . '/vendor/alikm6/php-pomo/l10n.php';
+
 require realpath(__DIR__) . '/functions/functions.php';
